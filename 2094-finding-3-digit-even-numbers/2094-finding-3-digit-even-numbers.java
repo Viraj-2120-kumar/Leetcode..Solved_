1536d1d2d24956d4cd6cbae782ecbaa21c0978d3
Viraj-2120-kumar <120045432+Viraj-2120-kumar@users.noreply.github.com>
2025-04-02 19:44:02 +0530

    04-02-2025 Easy .java 2094-finding-3-digit-even-numbers 18 ms (57.06%) 44.7 MB (81.17%)

--- 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.java
+++ 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.java
@@ -1,31 +1,54 @@
 class Solution {
     public int[] findEvenNumbers(int[] digits) {
-        // Count frequency of each digit (0-9)
-        int[] digitCounts = new int[10];
-        for (int d : digits) {
-            digitCounts[d]++;
-        }
+        HashMap<Integer,Integer> map=new HashMap<>();
         
-        List<Integer> result = new ArrayList<>();
-        
-        // Check all possible even 3-digit numbers
-        for (int num = 100; num < 999; num += 2) {
-            int a = num / 100;       // first digit
-            int b = (num / 10) % 10;  // second digit
-            int c = num % 10;         // third digit
-            
-            // Make a copy of counts to check against
-            int[] tempCounts = digitCounts.clone();
-            
-            // Check if we have enough of each digit
-            if (tempCounts[a]-- > 0 && 
-                tempCounts[b]-- > 0 && 
-                tempCounts[c]-- > 0) {
-                result.add(num);
+        int n=digits.length;
+         
+         //stroing the digits and frquency
+        for(int i=0;i<n;i++){
+            int key =digits[i];
+            if(map.containsKey(key)){
+                int freq= map.get(key);
+                map.put(key,freq+1);
             }
-        }
-        
-        // Convert list to array
-        return result.stream().mapToInt(i -> i).toArray();
+            else{
+                map.put(key,1);
+            }
+        } 
+         
+         //all threedigit number possible 100 to 999 only even
+         ArrayList<Integer> an = new ArrayList<>();
+         for(int i=100 ;i<=999;i+=2){
+            int x=i;
+            //finding digits abc =212 example
+            int c=x%10; x/=10;
+            int b=x%10; x/=10;
+            int a= x;
+            if(map.containsKey(a)){
+                int freqa= map.get(a);
+                map.put(a,freqa-1);
+                if(freqa==1)map.remove(a);
+                if(map.containsKey(b)){
+                    int freqb = map.get(b);
+                    map.put(b,freqb-1);
+                    if(freqb==1) map.remove(b);
+                        if(map.containsKey(c)){
+                            an.add(i);
+                        }
+                    map.put(b,freqb);
+                }
+                map.put(a,freqa);
+            }
+
+         }
+
+         int list[]= new int[an.size()];
+         for(int i=0;i<list.length;i++){
+            list[i]=an.get(i);
+         }
+
+         return list;
+
+
     }
 }
