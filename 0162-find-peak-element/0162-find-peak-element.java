d4b07f8b27764809b20fd36243b8eb28fb1340ef
Viraj-2120-kumar <120045432+Viraj-2120-kumar@users.noreply.github.com>
2025-04-18 15:41:12 +0530

    04-18-2025 Medium .java 0162-find-peak-element 0 ms (100%) 42.3 MB (73.13%)

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,20 +1,32 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        
-         int start =0;
-        int end=nums.length-1;
-        int mid=start+(end-start+1)/2;
-        while(start<end){
-            if (nums[mid] > nums[mid - 1]) {
-                start = mid;
-            } else {
-                end = mid - 1;
+        int n=nums.length;
+        if(n==1)return 0;
+
+         // As nums[-1] = nums[n] = -∞ that why
+        if(nums[0]>nums[1]) return 0;
+        if(nums[n-1]>nums[n-2]) return n-1;
+        //by doing this trim down the search space
+
+        int lo=1;
+        int hi=n-2;
+
+        while(lo<=hi){
+            int mid = lo+(hi-lo)/2;
+            if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]){
+                return mid;
             }
-            mid=start+(end-start+1)/2;
+            //mtlb yha tk increaing hai isliye peak aage milega
+            else if(nums[mid] > nums[mid-1] ){
+                lo=mid+1;
+
+            }
+            //ye check krega ki peak ke aage agay kya
+            else {
+                hi=mid-1;
+            }
         }
-        return end;
-
-        
+       
+           return -1; 
     }
-
 }
