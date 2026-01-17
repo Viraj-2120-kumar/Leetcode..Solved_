3485259077d375b2883c852588b18d14f2a8e342
Viraj-2120-kumar <120045432+Viraj-2120-kumar@users.noreply.github.com>
2026-01-17 10:34:44 +0530

    01-17-2026 Easy .java 0566-reshape-the-matrix 1 ms (61.15%) 47.7 MB (8.93%)

---
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[][] matrixReshape(int[][] mat, int r, int c) {
+        int m = mat.length;
+        int n = mat[0].length;
+
+        if (m * n != r * c) return mat;
+
+        int ans[][] = new int[r][c];
+        int index = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int row = index / c;
+                int col = index % c;
+                ans[row][col] = mat[i][j];
+                index++;
+            }
+        }
+
+        return ans;
+
+    }
+}
